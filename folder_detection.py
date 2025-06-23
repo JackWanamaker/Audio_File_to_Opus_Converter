feaf254dd56b8b7942b87fda9022e66d55e265d6
Ben Summer <37679388+JackWanamaker@users.noreply.github.com>
2025-06-22 23:41:24 -0400

    Added some new code for folder detection. I will pivot from using watchdog to simply using the os library and a timer to check periodically. Will use two JSON files to keep track of which folders have been processed, and which are waiting to be processed

---
+++ folder_detection.py
@@ -0,0 +1,27 @@
+import os
+import json
+from bisect import bisect_left
+import datetime
+import time
+
+path = "D:/"
+directories = [os.path.join(path, name) for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
+folders = [os.path.basename(item) for item in directories]
+created_date = [os.path.getctime(item) for item in directories]
+modified_date = [os.path.getmtime(item) for item in directories]
+
+new_data = [{"folder": folder, "modified-date": modified} for folder, modified in zip(folders,modified_date)]
+
+"""with open("data.json", "w") as f:
+    json.dump(data, f, indent=4)"""
+
+with open("data.json", "r") as f:
+    data = json.load(f)
+
+if len(data) != len(folders):
+    print("WTF")
+
+for i in range(len(data)):
+    if data[i]["folder"] != folders[i]:
+        print(folders[i])
+        print(data[i]["folder"])
--- main.py
+++ main.py
@@ -4,22 +4,46 @@ from watchdog.events import FileSystemEventHandler
 import time
 import base64
 
-command = [
+command0 = [
     "ffmpeg",
-    "-i", r"D:\Documents\Programming\Audio_Conversion\flac_meta_test.flac",
-    "-codec:a", "libopus",
-    "-b:a", "128k",
-    "-c:v", "copy",
-    r"D:\Documents\Programming\Audio_Conversion\flac_meta_test.opus"
+    "-i", r"D:\Documents\Programming\Audio_Conversion\01_Bathtub.flac",
+    "-f", "ffmetadata",
+    r"D:\Documents\Programming\Audio_Conversion\metadata.txt"
 ]
+
+command1 = [
+    "ffmpeg",
+    "-i", r"D:\Documents\Programming\Audio_Conversion\01_Bathtub.flac",
+    "-map", "0:v",
+    "-frames:v", "1",
+    r"D:\Documents\Programming\Audio_Conversion\cover.jpg"
+]
+
+def convert_to_base64():
+    with open(r"D:\Documents\Programming\Audio_Conversion\cover.jpg", "rb") as img_file:
+        b64_string = base64.b64encode(img_file.read()).decode('utf-8')
+    return b64_string
+
 
 class SimpleHandler(FileSystemEventHandler):
     def on_created(self, event):
         print(f"File created: {event.src_path}")
-        subprocess.run(command, check=True)
-        with open(r"D:\Documents\Programming\Audio_Conversion\cover.jpg", "rb") as img_file:
-            b64_string = base64.b64encode(img_file.read()).decode('utf-8')
-        print(b64_string)
+        #subprocess.run(command0, check=True)
+        #subprocess.run(command1, check=True)
+        #b64_string = convert_to_base64()
+        command2 = [
+            "ffmpeg",
+            "-i", r"D:\Documents\Programming\Audio_Conversion\01_Bathtub.flac",
+            "-i", r"D:\Documents\Programming\Audio_Conversion\metadata.txt",
+            "-map_metadata", "1",
+            "-codec:a", "libopus",
+            "-b:a", "128k",
+            r"D:\Documents\Programming\Audio_Conversion\01_Bathtub.opus"
+        ]
+
+        #meta_file = open(r"D:\Documents\Programming\Audio_Conversion\metadata.txt", "a")
+       # meta_file.write("METADATA_BLOCK_PICTURE=" + b64_string)
+        #subprocess.run(command2, check=True)
 
     def on_modified(self, event):
         print(f"File modified: {event.src_path}")
